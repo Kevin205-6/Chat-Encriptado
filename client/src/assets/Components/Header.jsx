da478d353d23836b3566f358c41d34d5f778e2bd
Kevin Pérez Escamilla <perez.kevin.1cv.2017@gmail.com>
2023-08-23 14:19:05 -0600

    Diseño de desencriptar y servidor

--- client/src/assets/Components/Header.jsx
+++ client/src/assets/Components/Header.jsx
@@ -3,7 +3,7 @@ import {Button} from 'react-bootstrap';
 function Header() {
     return (
     <header>
-        <a href="#">
+        <a href="http://localhost:5173/">
             <button className='btn btn-primary'>Reiniciar</button>
         </a>
     </header>);
--- main.js
+++ main.js
@@ -1,6 +1,10 @@
+import Servidor from './src/Servidor.js'
+import 'dotenv/config'
+
 
 function main() {
-
+    console.log(process.env.PORT);
+    const ser = new Servidor();
 }
 
 main();
